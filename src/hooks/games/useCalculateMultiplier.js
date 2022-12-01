faea2d97d56d11f3ceae7e7e8b63cef5d36601c7
Max Mogilski <maksmogi@gmail.com>
2022-12-01 15:27:46 +0100

    Logic of the scratch card game has been refactored

--- src/hooks/games/useCalculateMultiplier.js
+++ src/hooks/games/useCalculateMultiplier.js
@@ -1,65 +1,49 @@
 import { useCallback } from "react";
 
 export const useCalculateMultiplier = () => {
-	const getArryWithAmountOfItems = useCallback((items) => {
-		// getting array of all random game items
-		const arrayOfSingleItems = [...new Set(items.map((item) => item.name))];
-
-		const countArray = arrayOfSingleItems.map((item) => {
-			let multiplier;
-
-			items.forEach((itemToCheck) => {
-				if (itemToCheck.name === item) {
-					multiplier = itemToCheck.multiplier;
-				}
-			});
-			// making new object of single item
-			return { name: item, count: 0, multiplier };
-		});
-
-		// getting right amount of same items in game
+	const getCountedItems = (items) => {
+		const countedItems = {};
 		items.forEach((item) => {
-			for (let i = 0; i < countArray.length; i++) {
-				if (item.name === countArray[i].name) {
-					countArray[i].count++;
-				}
+			// To avoid the worst symbol
+			if (!item.multiplier) return;
+
+			if (countedItems[item.name]) {
+				if (countedItems[item.name].count === item.multiplier.length) return;
+
+				countedItems[item.name] = {
+					count: countedItems[item.name].count + 1,
+					multiplier: item.multiplier,
+				};
+			} else {
+				countedItems[item.name] = { count: 1, multiplier: item.multiplier };
 			}
 		});
-		return countArray;
+		return countedItems;
+	};
+
+	const getObjectWithAmountOfItems = useCallback((items) => {
+		return getCountedItems(items);
 	}, []);
 
 	// getting total multiplier from game
-	const calculateMultiplier = useCallback((arryWithAmountOfItems) => {
+	const calculateMultiplier = useCallback((objectWithAmountOfItems) => {
 		let multiplier = 0;
 
-		arryWithAmountOfItems.forEach((item) => {
-			if (!item.multiplier) return;
-
-			//check amount of items
-			switch (item.count.toString()) {
-				case "0":
-					return;
-				case "1":
-					multiplier += item.multiplier[0];
-					return;
-				case "2":
-					multiplier += item.multiplier[1];
-					return;
-				case "3":
-					multiplier += item.multiplier[2];
-					return;
-				case "4":
-					multiplier += item.multiplier[3];
-					return;
-				default:
-					multiplier += item.multiplier[3];
-					return;
+		for (const item in objectWithAmountOfItems) {
+			if (
+				objectWithAmountOfItems[item].count > 0 &&
+				objectWithAmountOfItems[item].multiplier
+			) {
+				multiplier +=
+					objectWithAmountOfItems[item].multiplier[
+						objectWithAmountOfItems[item].count - 1
+					];
 			}
-		});
+		}
 		return multiplier;
 	}, []);
 	return {
 		calculateMultiplier,
-		getArryWithAmountOfItems,
+		getObjectWithAmountOfItems,
 	};
 };
